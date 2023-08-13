583131faa0d5f18f78ad1052c0bc0530af576dd4
rechim25 <radu.echim.19@ucl.ac.uk>
2023-08-13 23:45:48 +0300

    Align text panel to any image size

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-document.getElementById('clickMe').addEventListener('click', function() {
-    document.getElementById('message').innerText = "Thanks for clicking!";
-});
+// Set a timeout to hide the GIF after 5 seconds (5000 milliseconds)
+setTimeout(() => {
+    document.getElementById('fullscreenGif').style.display = 'none';
+}, 2000);
+
+function repositionTextPanel() {
+    const img = document.getElementById('image');
+    const imgWidth = img.clientWidth;
+    const imgHeight = img.clientHeight;
+
+    const textPanel = document.getElementById('text-panel');
+    textPanel.style.top = `${imgHeight * 32 / 100}px`;
+    textPanel.style.left = `${imgWidth * 35 / 100}px`
+    textPanel.style.maxWidth = `${imgWidth * 25 / 100}px`
+}
+
+window.onload = repositionTextPanel;
+window.onresize = repositionTextPanel;
